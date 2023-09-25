fab46b488d78c1029cb5fc1bc68c2f82e64413de
Caden Morrison <64432691+caden-knight@users.noreply.github.com>
2023-09-25 01:25:39 -0600

    random spawn rate for apples
    *future caden* -> have a limited number of apples in game at once. ie only 3 apples can be in game at once

--- scripts/AppleTimer.cs
+++ scripts/AppleTimer.cs
@@ -6,9 +6,7 @@ public partial class AppleTimer : Timer
 	// gets apple resource to spawn later
 	public PackedScene appleRef = ResourceLoader.Load<PackedScene>("res://scenes/apple.tscn");
 	public Singleton singleton;
-	private float spawnRate = 3f; // in seconds
-
-
+	private float spawnRate = GD.RandRange(1, 5); // in seconds
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -21,12 +19,17 @@ public partial class AppleTimer : Timer
 	{
 		if (singleton.appleCollected)
 		{
+			// spawn new apple and position it
 			Apple apple = appleRef.Instantiate<Apple>();
 			AddChild(apple);
 			float xPos = GD.RandRange(0, 1152);
 			float yPos = GD.RandRange(0, 648);
 			apple.Position = new Vector2(xPos, yPos);
+
+			// switch back to false so too many apples don't spawn at once
 			singleton.appleCollected = false;
 		}
+
+		spawnRate = GD.RandRange(1, 5);
 	}
 }
